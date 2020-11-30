ec12e08eecb2b13396fa56e96cb62f2099e5dae3
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2020-11-30 14:54:28 +0000

    ..

--- src/test/java/GMI_BANK/runners/Runner_Omer.java
+++ src/test/java/GMI_BANK/runners/Runner_Omer.java
@@ -10,8 +10,9 @@ import org.junit.runner.RunWith;
                 "json:target/json-reports/cucumber.json",
                 "junit:target/xml-report/cucumber.xml"},
         features = "src/test/resources/features",
-        glue = "gmibank/stepdefinitions",
-        tags = "@db",
+        glue = "GMI_BANK/stepdefinitions",
+        tags = "@dnm",
+        strict = true,
         dryRun = false
 )
 public class Runner_Omer {
---
+++ src/test/java/GMI_BANK/stepdefinitions/deneme_step_defitions.java
@@ -0,0 +1,12 @@
+package GMI_BANK.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+
+public class deneme_step_defitions {
+
+    @Given("yazdir")
+    public void yazdir_o() {
+        System.out.println("yazdir");
+
+    }
+}
